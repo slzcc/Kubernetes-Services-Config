2227ce11396e013aeb80a799a6c33e39c38efcfa
slzcc <383821949@qq.com>
2020-09-23 21:03:32 +0800

    set Kubernetes Istio Configure

--- istio/set_tls.sh
+++ istio/set_tls.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+kubectl label namespace default istio-injection=enabled
 kubectl create secret tls istio-ingressgateway-certs --key domain.key --cert chained.pem -n istio-system
+
+kubectl edit svc -n istio-system istio-ingressgateway
